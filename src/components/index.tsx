9442fb4b8aabc6ee21f0870b2cd427038daf0d81
Erick Menezes <sr.tama@outlook.com>
2018-10-25 16:10:39 -0300

    added TypeScript

---
+++ src/components/index.tsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import styles from "./style";
+import { Text, View } from "react-native";
+
+type IProps = {
+  instructions: string;
+};
+
+export class RolePlay extends Component<IProps, {}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to Master RPG App!</Text>
+        <Text style={styles.instructions}>
+          To get started, Rolle Programming
+        </Text>
+        <Text style={styles.instructions}>{this.props.instructions}</Text>
+      </View>
+    );
+  }
+}
